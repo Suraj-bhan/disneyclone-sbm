c80142ffaf87888e296b340f87ce092ab4d3ca26
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-05-07 12:37:22 +0530

    added Login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   Link
 } from "react-router-dom";
 import Details from './components/Details';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
       <Router>
       <Header/>
       <Switch>
+        <Route path="/login">
+          <Login/>
+        </Route>
         <Route path="/details">
           <Details/>
         </Route>
